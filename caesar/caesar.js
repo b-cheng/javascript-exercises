37df9f15684094034d478696cec0a8ca68850c10
b-cheng <benc@teachable.com>
2019-01-25 17:17:47 -0500

    add plans for caeser exercise for later

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,8 @@
 const caesar = function() {
-
+	//possibly make an array of each letter as a const
+	//iterate through each letter of whatever word and shift it by however many letters
+		//requires for loop for however length the phrase or word is
+		//must skip punctuation and whitespace 
 }
 
 module.exports = caesar
