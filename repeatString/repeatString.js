49c9eb268ee48095825a0ea6c0ef129103ad3f71
b-cheng <benc@teachable.com>
2019-01-30 08:58:08 -0500

    Complete repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, numOfTimes) {
+	const finalWord=[]
+	if (numOfTimes < 0){
+		return 'ERROR'
+	} else {
+		for (var i = 0; i < numOfTimes; i++) {
+			finalWord.push(word)
+		}
+		return finalWord.join('')
+	}
 }
 
 module.exports = repeatString
