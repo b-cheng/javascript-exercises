44aa84eb41ac782cca9b40c32c5bcd6f164bd889
b-cheng <benc@teachable.com>
2019-01-31 09:09:22 -0500

    Complete reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+	const splitWord=word.split('')
+	const reverseWord=splitWord.reverse()
+	const finalWord=reverseWord.join('')
+	return finalWord
 }
 
 module.exports = reverseString
