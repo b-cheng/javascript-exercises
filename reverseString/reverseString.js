15de8911e3668292bab5dbc37ad870282f744e75
b-cheng <benc@teachable.com>
2019-01-31 09:10:16 -0500

    Refactor solution

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,8 +1,10 @@
 const reverseString = function(word) {
-	const splitWord=word.split('')
-	const reverseWord=splitWord.reverse()
-	const finalWord=reverseWord.join('')
-	return finalWord
+	const splitWord=word.split('').reverse().join('')
+	// const reverseWord=splitWord.reverse()
+	// const finalWord=reverseWord.join('')
+	// return finalWord
+	return splitWord
 }
 
 module.exports = reverseString
+
