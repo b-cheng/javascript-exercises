feac27588764427f75a0675d67078bd046e43a4a
b-cheng <benc@teachable.com>
2019-02-07 09:45:38 -0500

    Pass first test with solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+	if ((first < 0) || (!first.typeOf === 'number')) {
+		return 'ERROR'
+	} else if ((last < 0) || (!last.typeOf === 'number')) {
+		return 'ERROR'
+	} else {
+		var finalSum=0
+		for (var i=first; i <= last; i++) {
+			finalSum+=i
+		}
+		return finalSum
+	}
 }
 
 module.exports = sumAll
