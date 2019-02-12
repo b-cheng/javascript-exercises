c033cd2c18622261bdb9db75c9f71dac098b04f9
b-cheng <benc@teachable.com>
2019-02-12 17:57:22 -0500

    Add solution for tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+ //(f - 32) * 5/9ths
+	let c=(f-32)*(5/9)
+	return parseFloat(c.toFixed(1))
 }
 
-const ctof = function() {
-
+const ctof = function(c) {
+//c * 9/5ths + 32
+	let f=c*(9/5)+32
+	return parseFloat(f.toFixed(1))
 }
 
 module.exports = {
